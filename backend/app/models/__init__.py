4dd1f9a872bb3394b97a13f2b6e3babdfe277f4b
yarik-sg <tarik.seghiouer.yt@gmail.com>
2025-11-17 10:40:48 +0100

    Create project structure skeleton

--- backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.models.user import User
-from app.models.product import Product
-from app.models.favorite import Favorite
-from app.models.program import Program
-from app.models.gym import Gym
-
-__all__ = ["User", "Product", "Favorite", "Program", "Gym"]
